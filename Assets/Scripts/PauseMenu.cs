4f472ba218948cd812c82e5273dee21c28c2eb5d
Snack <92850241+Snackels@users.noreply.github.com>
2026-03-29 21:53:08 +0700

    Pause Menu Update

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+    [Header("References")]
+    [SerializeField] GameObject pausePopup;   // drag your popup panel here
+    [SerializeField] string mainMenuScene = "MainMenu";
+    [SerializeField] string gameScene = "SampleScene";
+
+    bool isPaused = false;
+
+    void Start() {
+        pausePopup.SetActive(false);
+    }
+
+    public void OnPauseButtonPressed() {
+        isPaused = !isPaused;
+
+        pausePopup.SetActive(isPaused);
+        Time.timeScale = isPaused ? 0f : 1f;
+    }
+
+    public void Restart() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(gameScene);
+    }
+
+    public void GoToMainMenu() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(mainMenuScene);
+    }
+}
